cfd0e5e70845980be45e82ca36e8e8a93ce5b757
MarchenkoRuslan <n3mo07@bk.ru>
2020-10-25 17:59:08 +0300

    Create post model

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 
-
-posts = [
-    {
-        'author': 'Ruslan M',
-        'title': 'First blog post',
-        'content': 'First post content',
-        'date_posted': '18 October, 2020',
-    },
-    {
-        'author': 'Roman M',
-        'title': 'Second blog post',
-        'content': 'Second post content',
-        'date_posted': '18 October, 2010',
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
