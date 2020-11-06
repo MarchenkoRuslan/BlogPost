ee5f6ddccc43d1910488a208ce8c5d0f6de07c7e
MarchenkoRuslan <n3mo07@bk.ru>
2020-11-06 23:49:04 +0300

    cloud media bucket

--- BlogPost/settings.py
+++ BlogPost/settings.py
@@ -147,3 +147,6 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_REGION_NAME = "eu-west-3"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
